a0be62ca876df15be9bcd4f9d7afd09765ff9bd2
Jingyuan Liu <toothacher17@users.noreply.github.com>
2019-03-20 20:41:25 -0400

    [deep-relation] add class to define data and basic code to read data (#3)
    thanks for the review

---
+++ dataprep/src/main/python/org/deeppacific/relation/dataprep/semieval/semieval-data.py
@@ -0,0 +1,40 @@
+""" This is the data model class for holding SemiEval-2010-task8 data """
+
+
+class SemiEvalData(object):
+
+    def __init__(self, record_id, raw_sentence, bag_of_words, entity1, entity2, relation):
+        """
+        class initiation function
+        Args:
+            record_id: the id of a record
+            raw_sentence: the raw sentence of the input
+            bag_of_words: a list of words
+            entity1: the first entity
+            entity2: the second entity
+            relation: the relation of the two entities
+        """
+        self.record_id = record_id
+        self.raw_sentence = raw_sentence
+        self.bag_of_words = bag_of_words
+        self.entity1 = entity1
+        self.entity2 = entity2
+        self.relation = relation
+
+    def get_record_id(self):
+        return self.record_id
+
+    def get_raw_sentence(self):
+        return self.raw_sentence
+
+    def get_bag_of_words(self):
+        return self.bag_of_words
+
+    def get_entity1(self):
+        return self.entity1
+
+    def get_entity2(self):
+        return self.entity2
+
+    def get_relation(self):
+        return self.relation
